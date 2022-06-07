ebc757f4ed499cb8b4919ae101a1ba56bd4a6040
kagg-design <info@kagg.eu>
2022-06-07 16:04:41 +0300

    A number of fixes and notes to the initial plugin realization.

--- src/php/Main.php
+++ src/php/Main.php
@@ -15,7 +15,7 @@ class Main {
 	/**
 	 * Top menu slug.
 	 */
-	public const GTS_MENU_SLUG = 'gts-translation-order';
+	public const GTS_MENU_SLUG = 'gts_translation_order';
 
 	/**
 	 * Sub menu slug.
@@ -57,6 +57,7 @@ class Main {
 		add_action( 'plugins_loaded', [ $this, 'init_text_domain' ] );
 		add_action( 'admin_enqueue_scripts', [ $this, 'admin_scripts' ] );
 		add_action( 'admin_menu', [ $this, 'menu_page' ] );
+		add_action( 'init', [ $this, 'create_table_order_table' ] );
 	}
 
 	/**
@@ -80,10 +81,10 @@ class Main {
 			wp_enqueue_script( 'bootstrap', '//cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js', [ 'jquery' ], '5.2.0', true );
 			wp_enqueue_style( 'bootstrap', '//cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css', '', '5.2.0' );
 			wp_enqueue_style( 'bootstrap-icon', GTS_TRANSLATION_ORDER_URL . '/vendor/twbs/bootstrap-icons/font/bootstrap-icons.css', '', '1.8.0' );
-			wp_enqueue_script( 'main', GTS_TRANSLATION_ORDER_URL . '/assets/js/admin/main.js', [ 'jquery' ], GTS_TRANSLATION_ORDER_VERSION, true );
+			wp_enqueue_style( 'admin-style', GTS_TRANSLATION_ORDER_URL . '/assets/css/admin/style.css', '', GTS_TRANSLATION_ORDER_VERSION );
 		}
 
-		wp_enqueue_style( 'admin-style', GTS_TRANSLATION_ORDER_URL . '/assets/css/admin/style.css', '', GTS_TRANSLATION_ORDER_VERSION );
+		wp_enqueue_script( 'main', GTS_TRANSLATION_ORDER_URL . '/assets/js/admin/main.js', [ 'jquery' ], GTS_TRANSLATION_ORDER_VERSION, true );
 	}
 
 	/**
@@ -127,7 +128,46 @@ class Main {
 	 *
 	 * @return void
 	 */
-	public function show_translation_cart(): void {
+	public function output_translation_cart(): void {
 		include GTS_TRANSLATION_ORDER_PATH . '/template/translation-cart-page.php';
+	}
+
+	/**
+	 * Create translation order table.
+	 *
+	 * @return void
+	 */
+	public function create_table_order_table(): void {
+		global $wpdb;
+
+		$table = get_option( 'gts_order_table_create' );
+
+		if ( ! $table ) {
+
+			// phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery
+			// phpcs:disable WordPress.DB.DirectDatabaseQuery.NoCaching
+			$sql = "CREATE TABLE `{$wpdb->prefix}gts_translation_order`  
+					(
+					    `id` BIGINT NOT NULL AUTO_INCREMENT , 
+					    `posts_id` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL , 
+					    `status` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL , 
+					    `total_cost` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL , 
+					    `date_send` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+					    `date_response` DATE NOT NULL , 
+					    `site_language` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL , 
+					    `target_languages` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL , 
+					    `industry` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL , 
+					    PRIMARY KEY (`id`)
+					);";
+
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+			dbDelta( $sql );
+
+			// phpcs:enable WordPress.DB.DirectDatabaseQuery.DirectQuery
+			// phpcs:enable WordPress.DB.DirectDatabaseQuery.NoCaching
+
+			update_option( 'gts_order_table_create', true );
+		}
 	}
 }
